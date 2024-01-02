f0554e27fbee996eed2df1c7065ce00566688bb4
LukaszMichalski03 <michalski469@gmail.com>
2024-01-03 00:15:58 +0100

    loginRegisterDemo

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BDProject_MarathonesApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+            
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string name, string username, string login, string password)
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
